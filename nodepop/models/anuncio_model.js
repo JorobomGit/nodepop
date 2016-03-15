65d557301f38197e209dd46ce7b986c5d475505b
Jose Luis Romero <joseluis.romero@babel.es>
2016-03-15 13:01:16 +0100

    Filtrado de precio y de venta finalizados

--- nodepop/models/anuncio_model.js
+++ nodepop/models/anuncio_model.js
@@ -19,9 +19,28 @@ anuncioSchema.statics.list = function(string, cb) {
     //Filtros
     var filtro = {};
 
-    if (string.tag !== undefined) filtro['tags'] = string.tag;   
-    if (string.venta == 'true' || string.venta == 'false') filtro['venta'] = string.venta;        
-    
+    if (string.tag !== undefined) filtro['tags'] = string.tag;
+    if (string.venta == 'true' || string.venta == 'false') filtro['venta'] = string.venta;
+    if (string.precio !== undefined) {
+        
+        var arr = string.precio.split("-");
+        //console.log(string.precio[0])
+        //if(arr[1] == '') arr[1] = undefined;
+        console.log(arr)
+
+
+        if (string.precio[0] == "-") //Caso -50
+            filtro['precio'] = { $lt: arr[1] };
+        else if (string.precio[string.precio.length-1] == "-") //Caso 50-
+            filtro['precio'] = { $gt: arr[0] };
+        else if (arr[1] != undefined) //Caso 1-50
+            filtro['precio'] = { $gt: arr[0], $lt: arr[1]};
+        else if (arr[0]>0) //Caso 50 y positivo
+            filtro['precio'] = arr[0];
+        else console.log('Precio no filtrado');
+        //Si no cumple ninguna condicion, no filtramos
+
+    }
 
     //filtro['nombre'] = 'Bicicleta';
     console.log(filtro);

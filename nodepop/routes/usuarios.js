89bc139d2ead2672fdb5b3b8b3e7764df079531e
Jose Luis Romero <joseluis.romero@babel.es>
2016-03-15 13:58:47 +0100

    Registro autenticacion comienzo

--- nodepop/routes/usuarios.js
+++ nodepop/routes/usuarios.js
@@ -4,6 +4,11 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Usuario = mongoose.model('Usuario');
+var auth = require('../lib/auth');
+
+
+var admin = 'admin';
+var pass = 'pass';
 
 /**
  * @api {get} /usuarios Get Users List sorted by specified (nombre as default).
@@ -20,7 +25,7 @@ var Usuario = mongoose.model('Usuario');
  *
  */
 
-router.get('/', function(req, res) {
+router.get('/', auth(admin,pass), function(req, res) {
 
     var sort = req.query.sort || 'nombre';
 
@@ -51,14 +56,23 @@ router.post('/', function(req, res) {
     //Instanciamos objeto en memoria
     console.log('body post: ', req.body);
     var usuario = new Usuario(req.body);
+    var query = Usuario.find(req.body); 
+    console.log(query);
+    //registro(usuario.nombre, usuario.email, usuario.clave);
+});
 
+function registro (nombre, email, clave) {
+    console.log(nombre);
+    console.log(email);
+    console.log(clave);
     //Lo guardamos en la BD
     usuario.save(function(err, newRow) {
         if (err) return res.json({ result: false, err: err });
         console.log(newRow);
         res.json({ result: true, row: newRow });
     });
+}
 
-});
+
 
 module.exports = router;

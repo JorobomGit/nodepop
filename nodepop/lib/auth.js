6e2caaeb29a50c57e626894c268d4e7a1dfa2a48
Jose Luis Romero <joseluis.romero@babel.es>
2016-03-16 11:51:50 +0100

    Autenticación a la hora de mostrar anuncios

--- nodepop/lib/auth.js
+++ nodepop/lib/auth.js
@@ -1,19 +1,39 @@
 'use strict';
 
 var basicAuth = require('basic-auth');
+var mongoose = require('mongoose');
+var Usuario = mongoose.model('Usuario');
 
-
-var fn = function(user, pass) {
+var fn = function() {
     return function(req, res, next) {
         var userRequest = basicAuth(req);
-        if (!userRequest || userRequest.name !== user || userRequest.pass !== pass) {
+        if (!userRequest) {
             console.log(userRequest);
             //Escribe algo en la cabecera
             res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
             res.send(401);
             return;
         }
-        next();
+
+        //Realizamos un find para ver si concuerda
+        var filtro = {};
+        filtro['nombre'] = userRequest.name;
+        filtro['clave'] = userRequest.pass;
+
+        var query = Usuario.find(filtro);
+        query.exec(function(err, rows) {
+            if (err) {
+                console.log(err)
+                return;
+            }
+            //Comprobamos que no hemos obtenido resultados
+            if (rows.length == 0) {
+                console.log('Login incorrecto');
+                res.send(401);
+                return;
+            }
+            next();
+        });
     };
 }
 
--- nodepop/routes/anuncios.js
+++ nodepop/routes/anuncios.js
@@ -6,11 +6,8 @@ var mongoose = require('mongoose');
 var Anuncio = mongoose.model('Anuncio');
 var auth = require('../lib/auth');
 
-var admin = 'admin';
-var pass = 'pass';
-
 /* GET anuncios list. */
-router.get('/', auth(admin,pass),  function(req, res) {
+router.get('/', auth(),  function(req, res) {
     Anuncio.list(req.query, function(err, rows) {
         if (err) return res.json({ result: false, err: err });
         //Cuando esten disponibles los mando en JSON
@@ -22,7 +19,7 @@ router.get('/', auth(admin,pass),  function(req, res) {
 //Añadir un anuncio
 router.post('/', function(req, res) {
     //Instanciamos objeto en memoria
-        console.log('body post: ' , req.body);
+    console.log('body post: ' , req.body);
     var anuncio = new Anuncio(req.body);
 
     //Lo guardamos en la BD
@@ -33,14 +30,5 @@ router.post('/', function(req, res) {
     });
 
 });
-
-
-/*Para filtros GET /messages?state=sent&archived=false
-Para ordenación GET /messages?sort=-priority,created_at
-Para paginación GET /users?skip=30&limit=10&returnTotal=true
-Para búsquedas GET /messages?q=frr149&sort=created_at*/
-
-
-
 
 module.exports = router;
